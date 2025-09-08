cde196d833cbbb5d2ed093ffd5ef928cdb0720af
magicweb <admin@mgwebgroup.com>
2025-09-08 10:41:28 +0000

    Changed yasumi submodule to the one from azuyalabs/yasumi instead of mgwebgroup.

--- lib/Autoloader.php
+++ lib/Autoloader.php
@@ -3,7 +3,7 @@ $loader = new Psr4AutoloaderClass;
 $loader->register();
 
 $loader->addNamespace('MGWeb', dirname(__DIR__).'/src');
-$loader->addNamespace('Yasumi', dirname(__DIR__).'/lib/mgweb/yasumi/src/Yasumi');
+$loader->addNamespace('Yasumi', dirname(__DIR__).'/lib/azuyalabs/yasumi/src/Yasumi');
 
 
 class Psr4AutoloaderClass
